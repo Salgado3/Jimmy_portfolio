bfffe52d7061599c39ce81b1133fdac0ba692f55
Salgado3 <jsalg10@gmail.com>
2025-06-18 02:22:31 -0500

    added mantine and photocards. wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { MainContainer } from "./components/MainContainer";
-
+import { MantineProvider } from "@mantine/core";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <MantineProvider>
       <MainContainer />
-    </>
+    </MantineProvider>
   );
 }
 
